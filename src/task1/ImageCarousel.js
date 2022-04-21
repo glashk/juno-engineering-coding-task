2bb29c689292c0a974d52e5a090ac7b23afb65dc
Levan Gvalia <84560189+glashk@users.noreply.github.com>
2022-04-21 12:41:45 +0400

    Update ImageCarousel.js

--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,67 @@
-import React from "react";
-import { fetchImageUrls } from "../api/index";
+import { useState, useEffect ,useCallback} from "react";
+import { fetchImageUrls, fetchImage } from "../api/index";
 
 const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+    const [imageIndex, setmageIndex] = useState(0)
+    const [imgUrl, setImgUrl] = useState('')
+    const [allImgUrl, setAllImgUrl] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    useEffect(async () => {
+        const urls = await fetchImageUrls()
+        setAllImgUrl(urls)
+    }, [])
+
+
+    const fetchImg = async () => {
+        setIsLoading(true)
+        const nextImg = await fetchImage(imageIndex)
+        setImgUrl(nextImg)
+        setIsLoading(false)
+    }
+    
+    useEffect(() => {
+       fetchImg()
+    }, [imageIndex])
+
+    const backHandler = () => { 
+        if(imageIndex === 0){
+            setmageIndex(allImgUrl.length - 1)
+        }else{ 
+            setmageIndex(imageIndex-1)
+        }
+    }
+
+    const nextHandler = () => { 
+        console.log(imageIndex === allImgUrl.length - 1);
+        if(imageIndex === (allImgUrl.length - 1)){
+            setmageIndex(0)
+        }else{
+            setmageIndex(imageIndex + 1)
+        }
+    }
+
+    return (
+        <>
+            <h1 >Your code goes here</h1>
+
+            <div className='main-container'>
+                <div onClick={backHandler}> Back </div>
+
+                <div className='imgContainer'>
+                    {isLoading ?
+                        <div class="loader" /> 
+
+                        : 
+                       <img className='imageee' src={imgUrl} />
+                        }
+                </div>
+                <div onClick={nextHandler}>Next</div>
+
+            </div>
+
+        </>
+    )
 };
 export default ImageCarousel;
